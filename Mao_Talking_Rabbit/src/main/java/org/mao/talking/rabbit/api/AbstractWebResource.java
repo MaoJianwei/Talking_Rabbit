aac31966799c673e6f21d92a87b1ec26cd5fcddf
Jianwei Mao <maojianwei2020@gmail.com>
2017-05-12 14:19:56 +0800

    this architect need servlet container

---
+++ Mao_Talking_Rabbit/src/main/java/org/mao/talking/rabbit/api/AbstractWebResource.java
@@ -0,0 +1,26 @@
+package org.mao.talking.rabbit.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by mao on 17-5-12.
+ */
+public abstract class AbstractWebResource {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    protected ObjectMapper getMapper() {
+        return mapper;
+    }
+
+    protected ObjectNode createObjectNode() {
+        return mapper.createObjectNode();
+    }
+
+    protected Response ok(Object obj) {
+        return Response.ok(obj).build();
+    }
+}
