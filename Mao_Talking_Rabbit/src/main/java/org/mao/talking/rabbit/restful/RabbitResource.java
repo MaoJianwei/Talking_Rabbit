3aa7385ab408543b0c4b1a9d800d74e07845c198
Jianwei Mao <maojianwei2020@gmail.com>
2017-05-30 10:49:08 +0800

    finish color API architecture

--- Mao_Talking_Rabbit/src/main/java/org/mao/talking/rabbit/restful/RabbitResource.java
+++ Mao_Talking_Rabbit/src/main/java/org/mao/talking/rabbit/restful/RabbitResource.java
@@ -3,8 +3,16 @@ package org.mao.talking.rabbit.restful;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.mao.talking.rabbit.api.AbstractWebResource;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -14,35 +22,41 @@ import javax.ws.rs.core.Response;
 public class RabbitResource extends AbstractWebResource {
 
     @GET
-    @Path("/testGet")
+    @Path("/color/{color}")
     @Produces("application/json") // necessary !!!
-    public Response testGetEvent() {
+    public Response testGetEvent(@PathParam("color") String color) {
 
-        // add to task Queue
-
-        ObjectNode data = getMapper().createObjectNode()
-                .put("success", 1080);
-
-        return ok(data);
-    }
-
-    @PUT
-    @Path("/testPut")
-    @Produces("application/json") // necessary !!!
-    @Consumes("application/json") // necessary !!!
-    public Response testPutEvent(ObjectNode body) {
-
-        // add to task Queue
-
-        ObjectNode data = getMapper().createObjectNode()
-                .put("success", 1080);
-
-        if(body != null) {
-            data.put("body", body.toString());
+        if(!checkColorInput(color)){
+            return ok(buildResult(1, "ok"));
         }
 
-        return ok(data);
+        color = calculateColor(color);
+
+        // add to task Queue
+
+        return ok(buildResult(200, "ok"));
     }
+
+    private ObjectNode buildResult(int code, String message){ return buildResult(code, message, null); }
+
+    private ObjectNode buildResult(int code, String message, Map<String, String> customFields) {
+        ObjectNode data = getMapper().createObjectNode()
+                .put("errcode", code)
+                .put("errmsg", message);
+
+        customFields.forEach((k, v) -> data.put(k,v));
+
+        return data;
+    }
+
+    private String calculateColor(String rawColor) {
+
+    }
+
+    private Boolean checkColorInput(String rawColor) {
+
+    }
+
 
     @POST
     @Path("/testPost")
@@ -58,20 +72,6 @@ public class RabbitResource extends AbstractWebResource {
         if(body != null) {
             data.put("body", body.toString());
         }
-
-        return ok(data);
-    }
-
-    @DELETE
-    @Path("/testDelete")
-    @Produces("application/json") // necessary !!!
-    //@Consumes("application/json") // DELETE can not consume data !!!
-    public Response testDeleteEvent() {
-
-        // add to task Queue
-
-        ObjectNode data = getMapper().createObjectNode()
-                .put("success", 1080);
 
         return ok(data);
     }
